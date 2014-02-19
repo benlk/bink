bc98b6e4a0f2de96046ee3e0f0be75786557e0db
Ben Keith <ben@benlk.com>
2014-02-19 00:31:47 -0500

    Cleaning up the front page
    I'd like to make something similar to Medium, but that increasingly
    looks like it will require abandoning subdiv.css
    https://github.com/Circa75/dropplets/blob/master/templates/simple/subdiv.css

--- custom/intro-buttons.php
+++ custom/intro-buttons.php
@@ -2,12 +2,6 @@
 	// Leave this file blank if you don't want buttons in your header. 
 ?>
 
-<p>
-	<ul class="links">
-	<!-- set manually in intro.php, because $intro_text doesn't have Markdown support yet-->
-		<a href="http://resume.benlk.com/"><li>R&eacute;sum&eacute;</li></a>
-		<a href="https://github.com/benlk/"><li>GitHub</li></a>
-		<a href="https://secure.flickr.com/photos/benlk"><li>Flickr</li></a>
-		<a href="https://twitter.com/benlkeith"><li>Twitter</li></a>
-	</ul>
-</p>
+<a href="http://resume.benlk.com/"><li>R&eacute;sum&eacute;</li></a>
+<a href="https://github.com/benlk/"><li>GitHub</li></a>
+<a href="https://secure.flickr.com/photos/benlk"><li>Flickr</li></a>
--- intro.php
+++ intro.php
@@ -2,22 +2,23 @@
     <div class="row">
         <div class="one-quarter meta">
 
-            <ul>
-                <li><?php echo $blog_title ?></li>
-                <li><a href="mailto:<?php echo $blog_email ?>?subject=Hello"><?php echo $blog_email ?></a></li>
-                <li><a href="http://twitter.com/<?php echo $blog_twitter ?>">&#64;<?php echo $blog_twitter ?></a></li>
-            </ul>
+            
         </div>
 
-        <div class="three-quarters post">
-            <h2><?php echo $intro_title ?></h2>
+        <div class="three-quarters">
+            <h1><?php echo $intro_title ?></h1>
 
             <p><?php echo $intro_text ?></p>
-            
-			<?php 
+			
+            <ul>
+                <li><a href="mailto:<?php echo $blog_email ?>?subject=Hello"><?php echo $blog_email ?></a></li>
+                <li><a href="http://twitter.com/<?php echo $blog_twitter ?>">&#64;<?php echo $blog_twitter ?></a></li>
+				<?php 
 				include "custom/intro-buttons.php" 
 				// These are if you want to put some buttons in your header. 
-			?>
+				?>
+            </ul>
+			
 			
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
         </div>
